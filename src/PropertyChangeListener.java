5f61c7f00c287bfd587d4a1674dd40abeaf8bfec
Iortoo <91682413+Iortoo@users.noreply.github.com>
2022-03-07 19:06:17 +0100

    listeners

--- src/PropertyChangeListener.java
+++
@@ -1,6 +0,0 @@
-import java.beans.PropertyChangeEvent;
-
-public interface PropertyChangeListener extends java.util.EventListener
-{
-  void propertyChange(PropertyChangeEvent evt);
-}
--- src/PropertyChangeSupport.java
+++
@@ -1,9 +0,0 @@
-import java.beans.PropertyChangeListener;
-
-public class PropertyChangeSupport
-{
-  public void addPropertyChangeListener(String name, PropertyChangeListener listener){};
-  public void addPropertyChangeListener(PropertyChangeListener listener){};
-  public void removePropertyChangeListener(String name, PropertyChangeListener listener){};
-  public void removePropertyChangeListener(PropertyChangeListener listener){};
-}
