623c8dab39b8f1f8bc037ea6fdf1ffc89896187a
Iortoo <91682413+Iortoo@users.noreply.github.com>
2022-03-07 19:53:26 +0100

    listeners

--- src/Heater.java
+++ src/Heater.java
@@ -1,6 +1,10 @@
-public class Heater
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class Heater implements PropertyChangeSubject
 {
   private HeaterSTATE currentState = new OffState();
+  private PropertyChangeSupport powerChangeSupport = new PropertyChangeSupport(this);
 
 
   public void turnUp(){
@@ -15,6 +19,31 @@ public class Heater
   public void setCurrentState(HeaterSTATE currentState)
   {
     this.currentState = currentState;
+    powerChangeSupport.firePropertyChange("PowerChanged",0,currentState.getPower());
+  }
+
+  @Override public void addPropertyChangeListener(String name,
+      PropertyChangeListener listener)
+  {
+    powerChangeSupport.addPropertyChangeListener(name,listener);
+  }
+
+  @Override public void addPropertyChangeListener(
+      PropertyChangeListener listener)
+  {
+    powerChangeSupport.addPropertyChangeListener(listener);
+  }
+
+  @Override public void removePropertyChangeListener(String name,
+      PropertyChangeListener listener)
+  {
+    powerChangeSupport.removePropertyChangeListener(name,listener);
+  }
+
+  @Override public void removePropertyChangeListener(
+      PropertyChangeListener listener)
+  {
+    powerChangeSupport.removePropertyChangeListener(listener);
   }
 }
 
