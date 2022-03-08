ab8559d2bae38a3c8ea7d2e2833644933eb2a2cc
Iortoo <91682413+Iortoo@users.noreply.github.com>
2022-03-08 14:49:59 +0100

    listeners

---
+++ src/heaterPowPosManageController.java
@@ -0,0 +1,12 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+public class heaterPowPosManageController
+{
+  @FXML public Label t;
+  @FXML public Label t0;
+  @FXML public ChoiceBox hPow;
+}
