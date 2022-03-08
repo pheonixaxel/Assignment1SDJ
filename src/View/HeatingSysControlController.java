f7dcc3dd5820d6da7866934fcb777aa925229305
Alex Cimpan <36129808+pheonixaxel@users.noreply.github.com>
2022-03-08 16:02:39 +0100

    alex

--- src/View/HeatingSysControlController.java
+++ src/View/HeatingSysControlController.java
@@ -1,5 +1,12 @@
+package View;
+
+import View.ViewHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class HeatingSysControlController
 {
@@ -13,8 +20,17 @@ public class HeatingSysControlController
   public Label t;
   @FXML
   public Label t0;
+  @FXML
+  public Button editButton;
 
-  Hea
+
+  ViewHandler viewHandler;
+
+  public void onEditButton(ActionEvent e) throws IOException
+  {
+    viewHandler.openView2();
+  }
+
 
   public void init()
   {
