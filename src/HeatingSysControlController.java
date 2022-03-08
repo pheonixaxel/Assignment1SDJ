f427edf06db77f067dedc811fe46666a89875e51
Alex Cimpan <36129808+pheonixaxel@users.noreply.github.com>
2022-03-08 15:42:51 +0100

    alex

--- src/HeatingSysControlController.java
+++ src/HeatingSysControlController.java
@@ -1,3 +1,23 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class HeatingSysControlController
 {
+  @FXML
+  public Label pos;
+  @FXML
+  public Label t2;
+  @FXML
+  public Label t1;
+  @FXML
+  public Label t;
+  @FXML
+  public Label t0;
+
+  Hea
+
+  public void init()
+  {
+
+  }
 }
